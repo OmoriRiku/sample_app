01e7f465659cab4d86a0c37827e868b11a37a493
EC2 Default User <ec2-user@ip-172-31-85-177.ec2.internal>
2023-02-02 07:22:47 +0000

    Sample_app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  # 投稿画面
   def new
     @list = List.new
   end
@@ -9,19 +10,30 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  # 一覧画面
   def index
     @lists = List.all
   end
 
+  # 詳細画面
   def show
     @list = List.find(params[:id])
   end
 
+  # 編集画面
   def edit
+    @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  # ストロングパラメータ
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  # 投稿画面
   get 'lists/new'
   post 'lists' => 'lists#create'
+  
+  # 一覧画面
   get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: "list"
-  get 'lists/edit'
+  
+  # 詳細画面
+  get 'lists/:id' => 'lists#show', as: 'list'
+  
+  # 編集画面
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
